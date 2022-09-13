2b036d7f228fe0208868554e289e6612560daa4f
izuchukwu-eric <onukwubeizu@gmail.com>
2022-09-14 00:51:50 +0100

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import React, { useState } from "react"
 import Stepper from '../components/Stepper'
 import StepperControl from '../components/StepperControl'
 import Account from '../components/steps/Account'
+import Details from '../components/steps/Details'
+import Final from '../components/steps/Final'
 
 
 const Home: NextPage = () => {
+  const [currentStep, setCurrentStep] = useState(1)
 
   const steps = [
     "Account Information",
@@ -16,6 +20,11 @@ const Home: NextPage = () => {
     switch(step) {
       case 1: 
         return <Account />
+      case 1: 
+        return <Details />
+      case 1: 
+        return <Final />
+      default:
     }
   }
 
@@ -23,7 +32,10 @@ const Home: NextPage = () => {
     <div className='md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white'>
       {/**stepper */}
       <div className='container horizontal mt-5'>
-        <Stepper />
+        <Stepper 
+          steps={steps}
+          currentStep={currentStep}
+        />
       </div>
       {/**navigation control */}
       <StepperControl />
