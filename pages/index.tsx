87fd6a273b8dee5570dc79fe03d7420d9650f8be
izuchukwu-eric <onukwubeizu@gmail.com>
2022-09-13 21:58:29 +0100

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next'
 import Stepper from '../components/Stepper'
 import StepperControl from '../components/StepperControl'
+import Account from '../components/steps/Account'
 
 
 const Home: NextPage = () => {
+
+  const steps = [
+    "Account Information",
+    "Personal Details",
+    "Complete"
+  ]
+
+  const displayStep = (step: any) => {
+    switch(step) {
+      case 1: 
+        return <Account />
+    }
+  }
+
   return (
     <div className='md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white'>
       {/**stepper */}
